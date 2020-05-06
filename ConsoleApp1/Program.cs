c751fcc9ff0a3ea42b226c44c354c15e0b23d336
Serdar Gülmez <48293914+Ausnachmit@users.noreply.github.com>
2020-05-07 01:56:57 +0300

    Güncellleme_2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("a");
+            Console.WriteLine("b");
             Console.ReadLine();
         }
     }
